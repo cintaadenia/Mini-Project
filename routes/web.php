86d53ede88ab978bf28b5cebdfa83bb885399f42
Awwoggaaa <ikandori154@gmail.com>
2024-11-19 22:13:53 +0700

    menata web.php

--- routes/web.php
+++ routes/web.php
@@ -8,30 +8,33 @@ use App\Http\Controllers\ResepController;
 use App\Http\Controllers\KunjunganController;
 use App\Http\Controllers\JadwalPraktekController;
 
-
 Route::get('/', function () {
     return view('dashboard');
 });
 
+// Apply the `auth` middleware to protect all routes except the dashboard
+Route::middleware('auth')->group(function () {
+    // Obat routes
+    Route::resource('obat', ObatController::class);
+
+    // Pasien routes
+    Route::resource('pasien', PasienController::class);
+
+    // Resep routes
+    Route::resource('resep', ResepController::class);
+
+    // Kunjungan routes
+    Route::resource('kunjungan', KunjunganController::class);
+
+    // Jadwal Praktek routes
+    Route::resource('jadwal_praktek', JadwalPraktekController::class);
+
+    // Dokter routes
+    Route::resource('dokter', DokterController::class);
+});
+
+// Authentication routes
 Auth::routes();
 
+// Home route (redirect after login)
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Obat routes
-Route::resource('obat', ObatController::class);
-
-// Pasien routes
-Route::resource('pasien', PasienController::class);
-
-// Resep routes
-Route::resource('resep', ResepController::class);
-
-// Kunjungan routes
-Route::resource('kunjungan', KunjunganController::class);
-
-// Jadwal Praktek routes
-Route::resource('jadwal_praktek', JadwalPraktekController::class);
-
-// Dokter routes
-
-Route::resource('dokter', DokterController::class);
