324b665f8f4e799bdf91cc6ae5375768f5245877
adityaandrew-15 <botsejati1506@gmail.com>
2024-11-20 11:05:39 +0700

    DAFVDASFDSF

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+@include('layouts.sidebar')
 <div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
