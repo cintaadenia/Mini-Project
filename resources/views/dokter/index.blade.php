aadba79050a7e14e0dd1a9c1931725af57d284b8
adityaandrew-15 <botsejati1506@gmail.com>
2024-11-20 08:12:16 +0700

    keren

--- resources/views/dokter/index.blade.php
+++ resources/views/dokter/index.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <title>Dokter</title>
-</head>
-
-<body>
     <div class="container">
         <h1>Dokter</h1>
         @if(session('success'))
@@ -165,7 +151,5 @@
             });
         });
     </script>
-</body>
-
-</html>
+ 
 @endsection
