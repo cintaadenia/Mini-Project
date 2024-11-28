899e30800dae00e82ecdcf2119216b4660fb3b93
hifnyy <m.hipni2019@gmail.com>
2024-11-28 10:46:13 +0700

    mengubah dashboard user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,27 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-
-                    <!-- Chart Section -->
+                    <div class="card mb-3" style="width: full height: 200px;">
+                        <div class="row no-gutters">
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">Selamat Datang {{ Auth::user()->name }}</h5>
+                              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet vero ducimus totam dicta cum consectetur reiciendis possimus libero dolor corporis repudiandae similique ipsam debitis voluptate enim iure quisquam, iusto voluptatibus.</p>
+                            </div>
+                          </div>
+                          <div class="col-md-4 d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('Medicio/assets/img/doctors/doctors-1.jpg') }}" class="card-img" alt="Profile Picture">
+                          </div>
+                        </div>
+                      </div>
+                    {{-- {{ __('You are logged in!') }} --}}
                     
                 </div>
             </div>
