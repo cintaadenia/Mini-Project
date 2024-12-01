5fa0d440c14ef664a61df5f4ed861759840241a9
Awwoggaaa <ikandori154@gmail.com>
2024-12-01 21:36:33 +0700

    pasien bisa input dan hanya muncul data yang dia input di tabel

--- database/migrations/2024_11_17_034842_create_pasiens_table.php
+++ database/migrations/2024_11_17_034842_create_pasiens_table.php
@@ -10,19 +10,18 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-
-{
-    Schema::create('pasiens', function (Blueprint $table) {
-        $table->id();
-        $table->string('nama');
-        $table->string('alamat');
-        $table->string('no_hp')->unique();
-        $table->date('tanggal_lahir');
-        $table->timestamps();
-    });
-}
-
-    
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Tidak perlu `after()`
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('no_hp')->unique();
+            $table->date('tanggal_lahir');
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
