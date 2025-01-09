b2f8c4de27a6e5f9201b37069e724059e33c132d
patt11111 <vanpatt946@gmail.com>
2025-01-09 07:19:27 +0700

    vhh

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -60,11 +60,11 @@
           </ul>
           <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
-        
+
         <a class="cta-btn" href="{{ Auth::check() ? route(Auth::user()->hasRole('admin') ? 'admin-home' : (Auth::user()->hasRole('dokter') ? 'home-dokter' : 'home')) : route('login') }}">
           {{ Auth::check() ? 'Home' : 'JOIN US' }}
       </a>
-      
+
 
       </div>
 
